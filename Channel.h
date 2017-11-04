60b667410838ae0cd035b99cd8936569c3cfc2f2
mlarroque <mlarroque@itba.edu.ar>
2017-11-04 11:36:14 -0300

    se agrega clase titular y se añade el correspondiente header en channel.h

---
+++ Channel.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "expat.h"
+#include "RSS.h"
+#include "titular.h"
+
+using namespace std;
+
+enum state {CH_TITLE, I_TITLE, I_UPBDATE, CHANNEL, ITEM, IDEL};
+
+class Channel
+{
+public:
+	Channel(const char * l = nullptr) : link(l) {};
+	~Channel();
+	void fetchTitles();
+	bool noError();
+	unsigned int getState();
+	const char * getErrorSt();
+	bool titlesEmpty();
+	bool newState(unsigned int newState);
+	
+	titular getNextTitular();
+
+private:
+	XML_Parser parse;
+	string chName;
+	string link;
+	string error;
+	unsigned int state;
+
+	titular tempTit;
+	vector <titular> titles;
+};
