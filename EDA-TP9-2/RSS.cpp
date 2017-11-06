6327fdc20fd0c80750e876daaf248d0dc8dc9291
Tomás Guillermo Londero Bonaparte <tlondero@itba.edu.ar>
2017-11-05 21:42:41 -0300

    Pequeño cambio en el constructor de RSS

--- EDA-TP9-2/RSS.cpp
+++ EDA-TP9-2/RSS.cpp
@@ -3,6 +3,8 @@
 RSS::RSS(string s)
 {
 	int i;
+	int j;
+	int k;
 	
 	for (i = 0; s[i] != BEGIN_OF_RSS; i++);			//Omito todo lo que se encuentra antes del RSS
 
@@ -14,18 +16,22 @@ RSS::RSS(string s)
 		path[i] = s[i];
 		i++;
 
-		for (int j = 0; j < END_OF_RSS_SIZE; j++)			//Tomo los siguientes 6 caracteres
+		for (j = 0; j < END_OF_RSS_SIZE; j++)			//Tomo los siguientes 6 caracteres
 		{
-			tempPath[j] = s[i+j];
+			tempPath[j] = s[i + j];
 		}
+
+		tempPath[j] = '\0';
 
 	} while (!strcmp(tempPath, END_OF_RSS));			//Chequea si se llego al fin del RSS
 
-	for(int k = 0; 0 < END_OF_RSS_SIZE; k++)			//Agrego el final del RSS
+	for(k = 0; 0 < END_OF_RSS_SIZE; k++)			//Agrego el final del RSS
 	{
 		path.resize(path.size() + 1);
-		path[i+k] = s[i+k];
+		path[i + k] = s[i + k];
 	}
+
+	path[i + k] = '\0';
 
 	size = path.size();
 	noError = true;
